2b8ed32c1c99ed38bd04db5155ae1956baeb360d
Edmilton Pansanato <edmilton.vinicius2@gmail.com>
2023-03-31 12:27:08 -0300

    ++ added chart into page work orders

--- src/components/statistic-work-order/index.tsx
+++ src/components/statistic-work-order/index.tsx
@@ -2,6 +2,8 @@ import { WorkOrderStatusEnum } from '@/types/enum/work-order-status.enum';
 import { CheckCircleFilled, ExclamationCircleFilled } from '@ant-design/icons';
 import { Card, Col, Row, Statistic } from 'antd';
 import styles from './statistic-work-order.module.scss';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 type StatisticWorkOrderProps = {
 	inProgress: number;
@@ -14,38 +16,88 @@ export const StatisticWorkOrder = ({
 	completed,
 	isLoading,
 }: StatisticWorkOrderProps) => {
+	const optionsChart: Highcharts.Options = {
+		chart: { type: 'pie', width: 500 },
+		title: { text: '' },
+		colors: ['#e19d9b', '#5fbe9f'],
+		series: [
+			{
+				colorByPoint: true,
+				data: [
+					{
+						name: WorkOrderStatusEnum.IN_PROGRESS.toUpperCase(),
+						y: inProgress,
+					},
+					{
+						name: WorkOrderStatusEnum.COMPLETED.toUpperCase(),
+						y: completed,
+					},
+				],
+				type: 'pie',
+			},
+		],
+		tooltip: {
+			pointFormat:
+				'{series.name} {series.id}: <b>{point.percentage:.1f}%</b>',
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+				},
+				showInLegend: false,
+			},
+		},
+		credits: {
+			enabled: false,
+		},
+	};
+
 	return (
-		<Row
-			gutter={16}
-			className={styles['statistic-work-order']}
-		>
-			<Col span={24}>
-				<h3>Statistics</h3>
-			</Col>
+		<>
+			<Row
+				gutter={16}
+				className={styles['statistic-work-order']}
+			>
+				<Col span={24}>
+					<h3>Statistics</h3>
+				</Col>
 
-			<Col span={8}>
-				<Card bordered={false}>
-					<Statistic
-						title={WorkOrderStatusEnum.IN_PROGRESS.toUpperCase()}
-						value={inProgress}
-						valueStyle={{ color: '#3f8600' }}
-						prefix={<ExclamationCircleFilled />}
-						loading={isLoading}
-					/>
-				</Card>
-			</Col>
+				<Col span={8}>
+					<Card bordered={true}>
+						<Statistic
+							title={WorkOrderStatusEnum.IN_PROGRESS.toUpperCase()}
+							value={inProgress}
+							valueStyle={{ color: '#cf1322' }}
+							prefix={<ExclamationCircleFilled />}
+							loading={isLoading}
+						/>
+					</Card>
+				</Col>
 
-			<Col span={8}>
-				<Card bordered={false}>
-					<Statistic
-						title={WorkOrderStatusEnum.COMPLETED.toUpperCase()}
-						value={completed}
-						valueStyle={{ color: '#cf1322' }}
-						prefix={<CheckCircleFilled />}
-						loading={isLoading}
+				<Col span={8}>
+					<Card bordered={true}>
+						<Statistic
+							title={WorkOrderStatusEnum.COMPLETED.toUpperCase()}
+							value={completed}
+							valueStyle={{ color: '#3f8600' }}
+							prefix={<CheckCircleFilled />}
+							loading={isLoading}
+						/>
+					</Card>
+				</Col>
+			</Row>
+
+			<Row>
+				<Col span={18}>
+					<HighchartsReact
+						highcharts={Highcharts}
+						options={optionsChart}
 					/>
-				</Card>
-			</Col>
-		</Row>
+				</Col>
+			</Row>
+		</>
 	);
 };
